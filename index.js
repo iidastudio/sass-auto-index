e04a093dbc43bbbaab7e1673589373fc8984c14e
iidastudio <philip.w.shotaro@gmail.com>
2021-02-01 23:29:54 +0900

    comment delete

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-/**
- * This module for dart-sass.
- * create some _index.scss by scanning in the specified directory.
- * Automatically write @forward in _index.scss.
- * 
- * notice) If you want to npm scripts watch, you need to avoid loops.
- * ex) "chokidar './src/assets/scss/' -c 'npm run css' -i /^.*index.scss$/ --initial"
- */
-
 const fs = require('fs');
 const path = require('path');
 
