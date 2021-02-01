a36dd98f86acf4108745b9ba57942e732636fb9e
iidastudio <philip.w.shotaro@gmail.com>
2021-02-01 22:32:23 +0900

    changed comment

--- sass-auto-index.js
+++ sass-auto-index.js
@@ -1,11 +1,9 @@
 /**
- * dart-sassの @use @forward を使いやすくするmodule
- * 指定ディレクトリの子・子孫ディレクトリ内で、_xxx.scssの形式でファイルを作成すると
- * _index.scssが自動で作成されその中に@forward 'xxx';の形式で自動記述される
- * 指定ディレクトリ直下では_xxx.scssファイルを作成しても_index.scssは作成されない
+ * This module for dart-sass.
+ * create some _index.scss by scanning in the specified directory.
+ * Automatically write @forward in _index.scss.
  * 
- * ※chokidar等の監視moduleを使用するとループしてしまうので_index.scssを監視対象からはずしてあげる必要がある
- * chokidarでは、-i '/hoge/' のように除外対象を正規表現で指定できるが、頭のアンダースコアが認識されないので'^.*index.scss$'として使用
+ * notice) If you want to npm scripts watch, you need to avoid loops.
  * ex) "chokidar './src/assets/scss/' -c 'npm run css' -i /^.*index.scss$/ --initial"
  */
 
@@ -20,13 +18,11 @@ class sassAutoIndex {
 
   readTarget = (targetPath) => {
     fs.readdir(targetPath, {withFileTypes: true}, (err, children) => {
-      // エラー処理
       if (err) {
         console.error('This Directory is undifind.');
         return;
       }
   
-      // ディレクトリが空だったら終了
       if (!children.length) {
         console.log('some empty directory exists.');
         return;
@@ -36,7 +32,6 @@ class sassAutoIndex {
   
         if (child.isFile() && targetPath !== process.argv[2] ) {
           let filename = child.name.toLowerCase();
-          // もし_index.scss以外のアンダースコアから始まるscssファイルだったらアンダースコアと拡張子を削除
           if( (filename.match(/^_.*\.(scss)$/) || filename.match(/^_.*\.(sass)$/)) && filename !== '_index.scss' ){
               this.fileNames.push(filename.slice(1, -5));
           }
@@ -62,18 +57,17 @@ class sassAutoIndex {
         }
       }
   
-      // ディレクトリ内のファイル名を_index.scss作成関数に渡して実行
+      // fileNames to makeIndex 
       if (this.fileNames.length) {
           this.makeIndex(this.fileNames, targetPath);
           this.fileNames = [];
       }
   
-      // ディレクトリ内にあったディレクトリをもう一度同じ処理
+      // Underlayer to readTerget()
       if (this.childrenDirPath.length) {
         for(const childDirPath of this.childrenDirPath) {
             this.readTarget(childDirPath);
         }
-        // １つのディレクトリを取得したら空にしないと溜まり続けて終わらなくなる
         this.childrenDirPath = [];
       }
       return;
