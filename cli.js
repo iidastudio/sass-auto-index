28ac428f5bcaae661c7341826a07fcfe1db54570
iidastudio <philip.w.shotaro@gmail.com>
2021-08-16 00:04:34 +0900

    add watch command

--- cli.js
+++ cli.js
@@ -1,5 +1,39 @@
-#!/usr/bin/env node
-'use strict';
-
+const chokidar = require('chokidar');
 const sai = require('./index');
-sai.readTarget(process.argv[2]);
+// sai.readTarget(process.argv[2]);
+
+// console.log(process.argv[2]);
+let watch;
+const argArray = process.argv.slice(2);
+
+argArray.forEach(arg => {
+  if(arg === "-w" || arg === "--watch") {
+    watch = true;
+    return;
+  } else {
+    watch = false;
+  }
+});
+
+console.log(argArray);
+
+if(watch === true) {
+  chokidar.watch(argArray, {ignored: [/[\/\\]\./, /._index\.scss/]})
+  .on('add', (path) => {
+    sai.readTarget(argArray[0]);
+  })
+  .on('addDir', (path) => {
+    sai.readTarget(argArray[0]);
+  })
+  .on('unlink', (path) => {
+    sai.readTarget(argArray[0]);
+  })
+  .on('unlinkDir', (path) => {
+    sai.readTarget(argArray[0]);
+  })
+  .on('error', (error) => {
+    console.error(error);
+  });
+} else {
+  sai.readTarget(argArray[0]);
+}
